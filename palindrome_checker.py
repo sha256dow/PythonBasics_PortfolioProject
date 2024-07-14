6c19d6ecf2ac1f5c143af14c3382ed15d610e4df
sha256dow <96244097+sha256dow@users.noreply.github.com>
2024-07-14 15:51:17 -0300

    palindrome_checker.py
    Just added a class named Palindrome due to organize the code even more.

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -1,13 +1,19 @@
 print("Is it a Palindrome? Let's check! Please enter the word you want to check...")
 
-def isItAPalindrome(): # Is it a palindrome? LOL
-    word = input('Word: ').upper()
-    word = word[::-1]
+class Palindrome:
+    def __init__(self) -> None:
+        pass
+        
+    def isItAPalindrome(): # Is it a palindrome? LOL
+        word = input('Word: ').upper()
+        #word = word[::-1]
 
-    if word == word[::-1]:
-        print('Palindrome')
-    else:
-        print('Not a palindrome')
+        if word == word[::-1]:
+            print('Palindrome')
+        else:
+            print(f'{word} is NOT a palindrome.')
     
-if __name__ == '__main__':
+
     isItAPalindrome() # returns
+
+    
